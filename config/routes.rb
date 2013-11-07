6293f6b6d71f2f051d386808e86a50b41d572cfc
komala <komala.geedimalla@gmail.com>
2013-11-07 14:29:55 +0530

    assets compiled for Heroku

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails3BootstrapDeviseCancan::Application.routes.draw do
+  match '/calendar(/:year(/:month))' => 'calendar#index', :as => :calendar, :constraints => {:year => /\d{4}/, :month => /\d{1,2}/}
+
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  root :to => "home#index"
+  devise_for :users
+  resources :users
+end
